a2a2146d701b9f8c6c9f20b382e50c4a94d4412a
joeylwang <joeylwang@wesure.cn>
2023-12-21 17:00:00 +0800

    基础功能开发完成

--- README.md
+++ README.md
@@ -1,71 +1,18 @@
-# git-branches README
-
-This is the README for your extension "git-branches". After writing up a brief description, we recommend including the following sections.
+# Git Branches
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+Provide a more convenient view of branch operations.
+This plugin defines a command `git-branches.all-branches` that can be used to list all branches in the repository, and then several most useful operation on them.
 
-For example if there is an image subfolder under your extension project workspace:
+## Usage
 
-\!\[feature X\]\(images/feature-x.png\)
+You can use it by typing `git-branches.all-branches` or `All Branches` in the command line.
+Or you can bind it to a keyboard shortcut, which I suggest.
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+## License
 
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
-
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
-
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+Feel free to do anything with this plugin.
+Glad it helps.
 
 **Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,141 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as child_process from 'child_process';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	// 获取当前打开的工作空间的根目录路径
+	const workspaceFolders = vscode.workspace.workspaceFolders;
+	if (!workspaceFolders) {
+		throw new Error('No workspace is open.');
+	}
+	const repoPath = workspaceFolders[0].uri.fsPath;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "git-branches" is now active!');
+	let disposable = vscode.commands.registerCommand('git-branches.all-branches', async () => {
+		try {
+			// 获取Git分支列表
+			const branches = await getGitBranches(repoPath);
+			const currentBranch = await execPromise('git rev-parse --abbrev-ref HEAD', { cwd: repoPath });
+			const items = branches.map(branch => ({
+				label: branch,
+				description: branch === currentBranch ? `*` : undefined
+			}));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('git-branches.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Git Branches!');
+			// 显示分支列表
+			const selectedItem = await vscode.window.showQuickPick(items, {
+				placeHolder: 'Select a git branch to operate on'
+			});
+			const selectedBranch = selectedItem?.label;
+
+			// 如果有选择，显示操作菜单
+			if (selectedBranch) {
+				const actions = ['checkout', 'merge', 'delete'];
+				const selectedAction = await vscode.window.showQuickPick(actions, {
+					placeHolder: 'Choose an action to perform on the branch'
+				});
+
+				// 执行选择的操作
+				if (selectedAction) {
+					await performGitAction(repoPath, selectedBranch, selectedAction, branches);
+				}
+			}
+		} catch (error) {
+			if (error instanceof Error) {
+				vscode.window.showErrorMessage(`Error: ${error.message}`);
+			} else {
+				vscode.window.showErrorMessage(`An unknown error occurred.`);
+			}
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+async function getGitBranches(repoPath: string): Promise<string[]> {
+	return new Promise<string[]>(async (resolve, reject) => {
+		// 在根目录下执行git命令
+		const gitCommand = 'git branch --list --all';
+		const branches = (await execPromise(gitCommand, { cwd: repoPath })).split('\n').filter(b => b).map(b => b.trim().replace('* ', ''));
+		resolve(branches);
+	});
+}
+
+async function performGitAction(repoPath: string, selectedBranch: string, action: string, branches: string[]) {
+	let gitCommand = '';
+
+	switch (action) {
+		case 'checkout':
+			gitCommand = await checkoutBranchCommand(repoPath, selectedBranch, branches);
+			break;
+		case 'merge':
+			gitCommand = `git merge ${selectedBranch}`;
+			break;
+		case 'delete':
+			if (selectedBranch.startsWith('remotes/')) {
+				let remoteBranch = selectedBranch.replace('remotes/', '');
+				gitCommand = `git push --delete ${remoteBranch.split('/')[0]} ${remoteBranch.split('/')[1]}`;
+			} else {
+				gitCommand = `git branch -D ${selectedBranch}`;
+			}
+			break;
+		default:
+			throw new Error('Unsupported action');
+	}
+
+	if (gitCommand) {
+		await vscode.window.withProgress({
+			location: vscode.ProgressLocation.Notification,
+			title: `Performing ${action} on ${selectedBranch}`,
+			cancellable: false
+		}, async (progress) => {
+			return new Promise<void>((resolve, reject) => {
+				child_process.exec(gitCommand, { cwd: repoPath }, (error, stdout, stderr) => {
+					if (error) {
+						vscode.window.showErrorMessage(`Git ${action} failed: ${stderr}`);
+						reject(error);
+					} else {
+						vscode.window.showInformationMessage(`Git ${action} successful: ${stdout}`);
+						resolve();
+					}
+				});
+			});
+		});
+	}
+}
+
+async function checkoutBranchCommand(repoPath: string, selectedBranch: string, branches: string[]): Promise<string> {
+	if (!selectedBranch.startsWith('remotes/')) {
+		return `git checkout ${selectedBranch}`;
+	}
+
+	// 提示用户输入本地分支名称
+	const defaultLocalBranchName = selectedBranch.replace(/^remotes\/[^\/]+\//, '');
+	const inputLocalBranchName = await vscode.window.showInputBox({
+		prompt: 'Enter a name for your new local branch',
+		value: defaultLocalBranchName,
+	});
+	if (!inputLocalBranchName) {
+		return '';
+	}
+
+	if (branches.includes(inputLocalBranchName)) {
+		// 如果本地分支已存在，切换到该分支
+		vscode.window.showInformationMessage(`Switching to existing branch ${inputLocalBranchName}.`);
+		return `git checkout ${inputLocalBranchName}`;
+	} else {
+		// 如果本地分支不存在，创建并切换到该分支
+		return `git checkout -b ${inputLocalBranchName} ${selectedBranch}`;
+	}
+}
+
+function execPromise(command: string, options: child_process.ExecOptions) : Promise<string> {
+	return new Promise((resolve, reject) => {
+		child_process.exec(command, options, (error, stdout, stderr) => {
+			if (error) {
+				reject(new Error(stderr));
+			} else {
+				resolve(stdout.trim());
+			}
+		});
+	});
+}
+
+export function deactivate() { }
